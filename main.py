50a06533f30e4ed897cbf323822c1148ab75c6f6
Diego C. <delcaos12@gmail.com>
2025-09-10 16:25:57 -0500

    Configuración puerto Render en main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 from routes.vehicle import vehicle_router
 from routes.auth import auth_router
 
@@ -15,8 +17,18 @@ app = FastAPI(
     ],
 )
 
+@app.get("/")
+def read_root():
+    return {"msg": "Hello 🚀"}
+
+if __name__ == "__main__":
+    import os, uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
 origins = [
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "https://castilloo.github.io/Client-ABComercial"
 ]
 
 app.add_middleware(
